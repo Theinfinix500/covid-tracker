10eb165f5c3f1e3add6a2ff21a920bacb42cbad0
theinfinix500 <jbaraali18@gmail.com>
2021-03-05 09:39:28 +0100

    did some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+import {ChartModule} from 'primeng/chart';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    ChartModule,
     AppRoutingModule
   ],
   providers: [],
